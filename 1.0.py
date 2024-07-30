53ae4f700298fc6b62132382fe81b785fc2ba023
billy-juicy <libertyeternity00@gmail.com>
2024-07-30 16:57:11 +0300

    Add main cycle

--- 1.0.py
+++ 1.0.py
@@ -1,17 +1,29 @@
-from random import *
+from random import randint
 
-number = randint(1, 100) # We generate any number from 1 to 100
+number = randint(1, 100) # Мы генерируем число от 1 до 100
 print("Добро пожаловать в числовую угадайку")
 
+# Функция корректности введенных данных
 def is_invalid(arg):
-    arg = int(input())
-    if arg.isdigit():
-        if 1 <= arg <= 100:
+    if arg.isdigit(): # Если значение состоит только из чисел, то прододолжаем работу кода
+        if 1 <= int(arg) <= 100:
             return True
         else:
             return False
     else:
         return False
-    
-arg = input()
-print(is_invalid(arg))
+
+# Основной цикл
+while True:
+    arg = input("Введите число от 1 до 100: ")
+    if is_invalid(arg):
+        guess = int(arg) # Преобразуем к целому числу
+        if guess < number:
+            print("Ваше число меньше загаданного, попробуйте еще разок")
+        elif guess > number:
+            print("Ваше число больше загаданного, попробуйте еще разок")
+        else:
+            print("Вы угадали, поздравляем!")
+            print("Спасибо, что играли в числовую угадайку. Еще увидимся...")
+    else:
+        print("А может быть все-таки введем целое число от 1 до 100?")
